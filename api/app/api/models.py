c168910158d49c8c896810d9bd50a6f2163b6933
Sebastian Granda Gallego <sgg10.develop@gmail.com>
2024-08-05 14:04:37 -0500

    feat: add error model response

---
+++ api/app/api/models.py
@@ -0,0 +1,23 @@
+from typing import Any, Dict, Optional
+
+from pydantic import Field, BaseModel
+
+
+class ErrorModel(BaseModel):
+    """Error Model for API responses."""
+
+    status: Optional[str | int] = Field(default="error")
+    message: str = Field(...)
+    error_type: str = Field(...)
+    details: Optional[Dict[str, Any]] = Field(None)
+
+    class Config:
+        extra = "ignore"
+        json_schema_extra = {
+            "example": {
+                "status": "error",
+                "message": "Error while processing request.",
+                "error_type": "INVALID_TASK_CONFIGURATION",
+                "details": {"detail": "detail"},
+            }
+        }
