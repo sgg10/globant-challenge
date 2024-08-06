a1717e7dbada8e0037da6354f894de16ccba4cee
Sebastian Granda Gallego <sgg10.develop@gmail.com>
2024-08-06 07:07:53 -0500

    update up.sh

--- up.sh
+++ up.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
 
+./replace_variables.sh
+
 echo "Starting containers..."
-docker-compose up -d --build
+docker compose up -d --build
 echo "Containers started."
 
 sleep 10
 
 echo "Creating topics..."
-docker-compose exec kafka kafka-topics --bootstrap-server kafka:9092\
+docker compose exec kafka kafka-topics --bootstrap-server kafka:9092\
     --create --topic globant-challenge --partitions 1 --replication-factor 1 --if-not-exists
 
 echo "Topics created."
